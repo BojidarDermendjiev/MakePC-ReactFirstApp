12887f63e8cf47496061336565924a3306800371
Bojidar Dermendjiew <bojidardermendjiew@abv.bg>
2024-07-28 18:20:24 +0300

    rename the i18next useTraslation

--- YouCanBuildIT.Demo/client/src/App.jsx
+++ YouCanBuildIT.Demo/client/src/App.jsx
@@ -2,11 +2,11 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import "./assets/styles/style.css";
-import { useTransition } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 
 function App() {
-  const { t, i18n } = useTransition();
+  const { t, i18n } = useTranslation();
 
   useEffect(() => {
     const lng = navigator.language;
