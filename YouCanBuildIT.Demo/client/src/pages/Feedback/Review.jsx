10268ed6cd223c8e32bc8ed0adad8bbbe5e26017
StroverIT <StroverIT>
2024-08-03 19:26:43 +0300

    removing name

--- YouCanBuildIT.Demo/client/src/pages/Feedback/Review.jsx
+++ YouCanBuildIT.Demo/client/src/pages/Feedback/Review.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "../../assets/styles/feedback.module.css";
 
-export default function Review({loggedInUser, email, comment, name}) {
+export default function Review({loggedInUser, email, comment}) {
   const isOwner = loggedInUser && loggedInUser.email === email;
   return (
     <div className={styles.card}>
-      <p className={styles.name}>{name}</p>
       <p className={styles.content}>{email}</p>
       <p className={styles.content}>{comment}</p>
       {isOwner && (
