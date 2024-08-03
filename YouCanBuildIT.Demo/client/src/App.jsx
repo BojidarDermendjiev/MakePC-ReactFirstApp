0ccd9a7dcd7e98b58b40c5ed4c7560f36e7ef1cd
StroverIT <StroverIT>
2024-08-03 14:51:23 +0300

    Removing unused data

--- YouCanBuildIT.Demo/client/src/App.jsx
+++ YouCanBuildIT.Demo/client/src/App.jsx
@@ -2,15 +2,14 @@ import "./assets/styles/style.css";
 import Main from "./components/Main/Main";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import AuthContextProvider from "./context/authContext";
 
 function App() {
   return (
-    <AuthContextProvider>
+    <>
       <Header />
       <Main />
       <Footer />
-    </AuthContextProvider>
+    </>
   );
 }
 
