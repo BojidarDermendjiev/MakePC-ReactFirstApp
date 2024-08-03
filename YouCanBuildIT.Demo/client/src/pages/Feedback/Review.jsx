8fd2acb9c63ef37e023a91a2f00ef164f66e7230
StroverIT <StroverIT>
2024-08-03 18:18:42 +0300

    Showing the proper data and now is without image

--- YouCanBuildIT.Demo/client/src/pages/Feedback/Review.jsx
+++ YouCanBuildIT.Demo/client/src/pages/Feedback/Review.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import styles from "../../assets/styles/feedback.module.css";
 
-export default function Review({
-  name,
-  password,
-  icon,
-  email,
-  feedback,
-  loggedInUser,
-}) {
+export default function Review({loggedInUser, email, comment, name}) {
   const isOwner = loggedInUser && loggedInUser.email === email;
   return (
     <div className={styles.card}>
-      <img src={icon} alt="user" />
       <p className={styles.name}>{name}</p>
       <p className={styles.content}>{email}</p>
-      <p className={styles.content}>{feedback.comment}</p>
+      <p className={styles.content}>{comment}</p>
       {isOwner && (
         <>
           <button>Edit</button>
