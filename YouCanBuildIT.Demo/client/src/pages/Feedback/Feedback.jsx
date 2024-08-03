267795a0cb822b3cee37ab71ec8b79fc05d0a533
StroverIT <StroverIT>
2024-08-03 17:21:35 +0300

    Show comments when is active user

--- YouCanBuildIT.Demo/client/src/pages/Feedback/Feedback.jsx
+++ YouCanBuildIT.Demo/client/src/pages/Feedback/Feedback.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../assets/styles/feedback.module.css";
 import { navigation } from "../../context/common/navigations";
@@ -6,6 +6,7 @@ import ErrorLoading from "../PageNotFound/ErrorLoading";
 import Spiner from "../PageNotFound/Spiner";
 import Review from "./Review";
 import useFetch from "../../hooks/useFetch";
+import { AuthContext } from "../../context/AuthContextProvider";
 
 export default function Feedback() {
   const {
@@ -14,6 +15,7 @@ export default function Feedback() {
     error,
   } = useFetch("http://localhost:3030/jsonstore/advanced/users");
 
+  const { user } = useContext(AuthContext);
   const loggedInUser = JSON.parse(localStorage.getItem("user"));
 
   if (error) return <ErrorLoading />;
@@ -37,11 +39,13 @@ export default function Feedback() {
           ))
         )}
       </div>
-      <div className={styles.coment}>
-        <Link className={styles.donation} to={navigation.getCommentFromUrl()}>
-          Comment
-        </Link>
-      </div>
+      {user && (
+        <div className={styles.coment}>
+          <Link className={styles.donation} to={navigation.getCommentFromUrl()}>
+            Comment
+          </Link>
+        </div>
+      )}
       <div className={styles.btn}>
         <h1>Buy me a coffee 😊</h1>
         <Link className={styles.donation} to={navigation.getDonationUrl()}>
