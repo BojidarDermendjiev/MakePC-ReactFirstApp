7505348042f1a440ba12bd2c92dae3ba3b0596e2
BojidarDermendjiev <bojidardermendjiew@abv.bg>
2024-08-03 18:03:31 +0300

    Upload comment page

--- YouCanBuildIT.Demo/client/src/pages/Feedback/Comment.jsx
+++ YouCanBuildIT.Demo/client/src/pages/Feedback/Comment.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useContext, useState } from "react";
+import styles from "../../assets/styles/comment.module.css";
+import { AuthContext } from "../../context/AuthContextProvider";
 
 export default function Comment() {
+  const { user } = useContext(AuthContext);
+  const [comment, setComment] = useState("");
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
+
   return (
-    <div>
-      <p>Comment!</p>
+    <div className={styles["comment-form-container"]}>
+      <div className={styles["form-group"]}>
+        <label htmlFor="comment">Comment</label>
+        <textarea
+          id="comment"
+          name="comment"
+          rows="4"
+          value={comment}
+          onChange={handleCommentChange}
+          required
+        ></textarea>
+      </div>
+      <button type="submit">Apply</button>
     </div>
   );
 }
