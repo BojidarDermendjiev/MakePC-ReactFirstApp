561496287b2527b60a9dddc36ec665521e1a1e56
StroverIT <StroverIT>
2024-08-03 13:32:15 +0300

    Removing vite config

--- YouCanBuildIT.Demo/client/vite.config.js
+++ YouCanBuildIT.Demo/client/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { config } from 'dotenv';
-
-// Load environment variables from .env file
-config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // eslint-disable-next-line no-undef
-    'process.env': process.env
-  }
+  
 });
