346b39a734cfb0e1b0ba1125e9540a963be7618c
StroverIT <StroverIT>
2024-08-03 12:52:31 +0300

    Adding AuthProvider with domain and client it secret from dotenv

--- YouCanBuildIT.Demo/client/src/App.jsx
+++ YouCanBuildIT.Demo/client/src/App.jsx
@@ -3,13 +3,21 @@ import Main from "./components/Main/Main";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
 function App() {
   return (
-    <>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH_DOMAIN}
+      clientId={import.meta.env.VITE_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <Header />
       <Main />
       <Footer />
-    </>
+    </Auth0Provider>
   );
 }
 
